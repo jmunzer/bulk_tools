712c6f0e010e9999543ad2c63037ac805219bcb0
jmunzer <63249152+jmunzer@users.noreply.github.com>
2020-04-16 10:06:43 +0100

    tools merge

---
+++ para_upd/del/src/comp.php
@@ -0,0 +1,265 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "<p>Starting</p>";
+
+//*****************GRAB_INPUT_DATA**********
+
+$uploaddir = 'uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+	echo "File is valid, and was successfully uploaded.\n";
+} else {
+	echo "File is invalid, and failed to upload - Please try again. -\n";
+}
+echo "</br>";
+print_r($uploadfile);
+echo "</br>";
+echo "</br>";
+
+$shortCode = $_REQUEST['SHORT_CODE'];
+
+echo "Tenancy Shortcode set: " . $shortCode;
+echo "</br>";
+
+$clientID = $_REQUEST['CLIENT_ID'];
+
+echo "Client ID set: " . $clientID;
+echo "</br>";
+
+$secret = $_REQUEST['CLIENT_SEC'];
+
+echo "Client secret set: " . $secret;
+echo "</br>";
+
+$TalisGUID = $_REQUEST['GUID'];
+
+echo "User GUID to use: " . $TalisGUID;
+echo "</br>";
+echo "</br>";
+
+//**********CREATE LOG FILE TO WRITE OUTPUT*
+
+$myfile = fopen("output.log", "a") or die("Unable to open output.log");
+fwrite($myfile, "Started | Input File: $uploadfile | Date: " . date('d-m-Y H:i:s') . "\r\n\r\n");
+fwrite($myfile, "List name" . "\t" . "List ID" . "\t" . "Item UUID" . "\t" . "Item added" . "\t" . "List Published" . "\r\n");
+
+//************SET_VARIABLES***********
+//uncomment if you want to set these permanently.. good idea tbh!
+/*
+	$shortCode = "";
+	$clientID = "";
+	$secret = "";
+	$TalisGUID = "";
+*/
+
+$tokenURL = 'https://users.talis.com/oauth/tokens';
+$content = "grant_type=client_credentials";
+
+//*********GET DATE**********************
+
+$date = date('Y-m-d\TH:i:s');
+// $date1 = "2015-12-21T15:44:36";
+
+//************GET_TOKEN***************
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $tokenURL);
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_USERPWD, "$clientID:$secret");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+
+$return = curl_exec($ch);
+$info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+if ($info !== 200){
+	echo "<p>    ERROR: There was an error getting a token:</p><pre>" . var_export($return, true) . "</pre>";
+} else {
+	echo "    Got Token</br>";
+}
+
+curl_close($ch);
+
+$jsontoken = json_decode($return);
+
+if (!empty($jsontoken->access_token)){
+	$token = $jsontoken->access_token;
+} else {
+	echo "<p>    ERROR: Unable to get an access token</p>";
+	exit;
+}
+
+
+//***********READ**DATA******************
+
+$file_handle = fopen($uploadfile, "rb");
+
+while (!feof($file_handle) )  {
+
+	$line_of_text = fgets($file_handle);
+	$parts = explode(" ", $line_of_text);
+	$barc = trim($parts[0]);
+	$item_lookup = 'https://rl.talis.com/3/' . $shortCode . '/draft_items/' . $barc . '?include=list';
+
+	//************GRAB**LIST**DETAILS*************
+
+		$ch4 = curl_init();
+
+		curl_setopt($ch4, CURLOPT_URL, $item_lookup);
+		curl_setopt($ch4, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch4, CURLOPT_HTTPHEADER, array(
+	
+			"X-Effective-User: $TalisGUID",
+			"Authorization: Bearer $token",
+			'Cache-Control: no-cache'
+	
+		));
+		$output4 = curl_exec($ch4);
+
+  		$info4 = curl_getinfo($ch4, CURLINFO_HTTP_CODE);
+		$output_json2 = json_decode($output4);
+		curl_close($ch4);
+
+		if ($info4 !== 200){
+			echo "<p>ERROR: There was an error getting the draft item information:</p><pre>" . var_export($output, true) . "</pre>";
+			continue;
+		} else {
+			echo "    Got item draft information</br></br>";
+		}
+
+		$assoc_listid = $output_json2->included[0]->id;
+		echo "    list_id: " . $assoc_listid . "</br>";
+		$title = $output_json2->included[0]->attributes->title;
+		echo "    Title: " . $title . "</br>";
+		$etag = $output_json2->included[0]->meta->list_etag;
+		echo "    ETag: " . $etag . "</br>";
+		
+
+	//**************DELETE_ITEM***************
+	$patch_url = 'https://rl.talis.com/3/' . $shortCode . '/draft_items/' . $barc;
+
+	$input = '	{
+					"meta": {
+						"list_etag": "' . $etag . '",
+						"list_id": "' . $assoc_listid . '"
+					}
+				}';
+
+	//**************POST_THE_DELETE************
+
+	$ch2 = curl_init();
+
+	curl_setopt($ch2, CURLOPT_URL, $patch_url);
+	curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'DELETE');
+	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch2, CURLOPT_HTTPHEADER, array(
+		
+		"X-Effective-User: $TalisGUID",
+		"Authorization: Bearer $token",
+		'Cache-Control: no-cache'
+	));
+
+	curl_setopt($ch2, CURLOPT_POSTFIELDS, $input);
+
+	
+	$output2 = curl_exec($ch2);
+	$info2 = curl_getinfo($ch2, CURLINFO_HTTP_CODE);
+
+	curl_close($ch2);
+	if ($info2 !== 200){
+		echo "<p>ERROR: There was an error deleting the item:</p><pre>" . var_export($output2, true) . "</pre>";
+		fwrite($myfile, "Item not deleted - failed" . "\t");
+		continue;
+	} else {
+		echo "    Deleted item $barc from list $assoc_listid</br>";
+		fwrite($myfile, "Item deleted successfully" . "\t");
+	}
+
+	//************GRAB**AN**ETAG**AGAIN*************
+
+	$list_lookup = 'https://rl.talis.com/3/' . $shortCode . '/draft_lists/' . $assoc_listid;
+
+	$ch5 = curl_init();
+
+	curl_setopt($ch5, CURLOPT_URL, $list_lookup);
+	curl_setopt($ch5, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch5, CURLOPT_HTTPHEADER, array(
+
+		"X-Effective-User: $TalisGUID",
+		"Authorization: Bearer $token",
+		'Cache-Control: no-cache'
+
+	));
+	$output5 = curl_exec($ch5);
+	$info5 = curl_getinfo($ch5, CURLINFO_HTTP_CODE);
+	$output_json3 = json_decode($output5);
+	curl_close($ch5);
+
+	$etag2 = $output_json3->data->meta->list_etag;
+	echo "    Updated ETag: " . $etag2 . "</br>";
+	echo "    ---------------------------------------------------";
+	echo "</br>";
+
+	//**************PUBLISH**LIST***************
+	$patch_url2 = 'https://rl.talis.com/3/' . $shortCode . '/draft_lists/' . $assoc_listid . '/publish_actions';
+	$input2 = '{
+				"data": {
+					"type": "list_publish_actions"
+				},
+				"meta": {
+					"has_unpublished_changes": "true",
+					"list_etag": "' . $etag2 . '",
+					"list_id": "' . $assoc_listid . '"
+				}
+			}';
+
+	//**************PUBLISH POST*****************
+
+	$ch3 = curl_init();
+
+	curl_setopt($ch3, CURLOPT_URL, $patch_url2);
+	curl_setopt($ch3, CURLOPT_CUSTOMREQUEST, 'POST');
+	curl_setopt($ch3, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch3, CURLOPT_HTTPHEADER, array(
+
+		"X-Effective-User: $TalisGUID",
+		"Authorization: Bearer $token",
+		'Cache-Control: no-cache'
+	));
+
+	curl_setopt($ch3, CURLOPT_POSTFIELDS, $input2);
+
+
+	$output3 = curl_exec($ch3);
+	$info3 = curl_getinfo($ch3, CURLINFO_HTTP_CODE);
+	curl_close($ch3);
+	if ($info3 !== 202){
+		echo "<p>ERROR: There was an error publishing the list:</p><pre>" . var_export($output3, true) . "</pre>";
+		fwrite($myfile, "Publish failed" . "\t");
+		continue;
+	} else {
+		echo "    Published changes to $assoc_listid </br>";
+		fwrite($myfile, "Published successfully" . "\t");
+	}
+
+	fwrite($myfile, "\n");
+	echo "End of Record.";
+	echo "---------------------------------------------------</br></br>";
+}
+
+fwrite($myfile, "\r\n" . "Stopped | End of File: $uploadfile | Date: " . date('d-m-Y H:i:s') . "\r\n");
+
+fclose($file_handle);
+fclose($myfile);
+
+print("</br></br><a href='index.html'>Back</a>");
+
+?>
---
+++ para_upd/item/README.md
@@ -0,0 +1,76 @@
+# PARA_UPD - add_item
+
+## Running with XAMPP
+
+This guide will instruct you how to get this paragraph updater working on a personal/work computer providing you have administrative rights enabled.
+
+## Installer
+
+- First download & run XAMPP.
+
+        This can be found at https://www.apachefriends.org/
+         - Download the latest version for your operating system of choice.
+
+- Once in the installer you will be prompted to 'Select Components'.
+
+    Untick everything besides the following:
+
+            Apache
+            PHP
+
+- Install Location
+
+        Install XAMPP to "C:\xampp" if on Windows.
+
+- Windows Firewall
+
+    Depending on your Windows security settings, you may receive a Windows Security Alert from Windows Defender Firewall asking for Apache HTTP Server to communicate on certain networks. If you do, ensure that the following network is ticked:
+
+        # Private networks, such as my home or work network
+
+## Running the XAMPP environment
+
+- Once XAMPP has successfully installed, please run this service by doing the following:
+
+        1) Click on the start menu
+        2) Type 'xampp'
+        3) Select 'XAMPP Control Panel'
+
+- Once the XAMPP Control Panel is running, start the Apache service by clicking 'Start' under Actions. If successful, you should see the following:
+
+        10:49:53  [main] 	Control Panel Ready
+        10:49:56  [Apache] 	Attempting to start Apache app...
+        10:49:56  [Apache] 	Status change detected: running
+        
+    ...and the Apache module will be highlighted in green.
+
+## Putting the tool in the right place
+
+- Download the script from: https://github.com/jmunzer/para_upd/archive/master.zip
+- Extract the downloaded ZIP file to the following location: c:\xampp\htdocs
+
+## Running the tool
+
+- click on: http://localhost/para_upd-master/src/index.html
+- Follow step on webform.
+- An example upload file is included as list_id.txt and looks like this (one list ID per line):
+
+        5AF6B766-325D-A542-5837-7AD6DC4F703E
+        5D6EA0C2-45F4-268B-878B-0BEBF1C234E9
+        CB98F155-576C-553D-7C37-88D1A28CAF49
+        45146B68-9C56-92B0-AE28-0AB0D5A5239D
+        EAFDDB7B-B580-6FDD-8808-AE63781EB866
+        C3FECB4D-1509-60A3-BCB3-6E4142917C3A
+        B6E658B0-5DAE-CF98-5FAA-5DB345685320
+        C2D6D98F-5523-1A8D-BA00-5ED7114C3E9A
+        BC011E42-A395-9248-E147-1C869BFECF02
+        185251D1-23F3-F0CF-00C7-286D4645F07C
+
+List IDs can be acquired from the TARL 'All Lists' report by grabbing the unique ID from the end of each List Link:
+
+    http://yorksj.rl.talis.com/lists/5AF6B766-325D-A542-5837-7AD6DC4F703E
+
+## Report Files
+
+- Report files are under the name and root folder of ./output.log
+- If you extracted the tool to the suggestioned location in the above steps, this will be: c:\xampp\htdocs\para_upd-master\src\output.log
