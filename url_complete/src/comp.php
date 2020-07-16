03a888c7af8394944b1cd71e025c884dc3ed859a
jomnz <jmunzer.work@gmail.com>
2020-07-16 14:23:40 +0100

    replace functionality working and tested

--- url_complete/src/comp.php
+++ url_complete/src/comp.php
@@ -296,6 +296,7 @@ function delete_url($itemID, $oldURL, $shortCode, $TalisGUID, $token) {
 		} else {
 			increment_counter('Delete operations with no web addresses on item: ');
 		}
+		increment_counter('URLs deleted: ');
 	}
 }
 
@@ -303,30 +304,35 @@ function replace_url($itemID, $oldURL, $newURL, $shortCode, $TalisGUID, $token){
 	global $myfile;
 	global $shouldWritetoLive;
 	echo "replace</br>";
-
+	
 	$resource_data = get_item($shortCode, $itemID, $TalisGUID, $token);
 	if($resource_data) {
 		// get the existing web addresses
 		$resource_id = get_resource_id($resource_data);
 		$web_address_array = get_webaddress_array($resource_data);
-		
-		if ($web_address_array) {
-			// add a new web addresses to the existing ones
-			$web_address_array = check_web_addresses($oldURL, $newURL, $web_address_array, "replace");
-			// build the PATCH body
-			$body = build_patch_body($resource_id, $web_address_array, $newURL);
-			
-			// if not a dry run - update
-			if ($shouldWritetoLive == "true") {
-				post_url($shortCode, $resource_id, $body, $TalisGUID, $token);
-			} else {
-				fwrite($myfile, "Resource URL Not Updated - Dry Run");
-			}
-			increment_counter('URLs replaced: ');
+		// add a new web addresses to the existing ones
+		$web_address_array = check_web_addresses($oldURL, $newURL, $web_address_array, "replace");
+		// build the PATCH body
+		$body = build_patch_body($resource_id, $web_address_array, $newURL);
+		/* don't think we need to check online resource for a replace
+		$online_resource = get_online_resource($resource_data);
+		$body = check_online_resource($oldURL, $online_resource, $body);
+		*/
+		// if not a dry run - update
+		if ($shouldWritetoLive == "true") {
+			post_url($shortCode, $resource_id, $body, $TalisGUID, $token);
 		} else {
-			increment_counter('Replace operations with no web addresses on item: ');
+			fwrite($myfile, "Resource URL Not Updated - Dry Run");
 		}
+		increment_counter('URLs replaced: ');
 	}
+	// get the item
+	// get the existing web addresses
+	// check that the web address to replace is present
+	// remove the old and the new
+	//$web_addresses[$oldURL_index] = $newURL;  // update the found address
+	// update the online resource
+	// if not a dry run - update
 }
 /*
 function echo_message_to_screen($log_level, $message){
